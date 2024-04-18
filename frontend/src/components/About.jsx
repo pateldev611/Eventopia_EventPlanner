6f9b0926c65be030db6396a18ad695b9938e23b4
Dev <devpatel0611@gmail.com>
2024-04-18 18:41:14 +0530

    Initial Commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      From grand galas to intimate gatherings, our event planning website transforms visions into memorable occasions. With seamless navigation, users explore a plethora of services, from venue selection to catering options, tailored to their needs. Our platform fosters collaboration, connecting clients with expert planners and vendors. Captivating designs and intuitive tools streamline the planning process, empowering users to create events that reflect their unique style. Whether orchestrating weddings, corporate functions, or social soirees, our site serves as the ultimate companion, guiding every step with precision and flair. Let's turn dreams into realities, one event at a time.
+      </p>
+    </section>
+  );
+};
+
+export default About;
